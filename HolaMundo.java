19c048d3d502f9f841f384d41419b1009c4b8ea2
ElZeroDay <68492807+fraisecup@users.noreply.github.com>
2025-11-04 16:44:02 -0500

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,17 @@
+//Cada programa tiene al menos una clase y dentro de ella un método main, que es donde todo comienza.
+
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola, Alejandro!");
+// Variables
+        int edad = 21;          // entero
+        double altura = 1.75;   // número con decimales
+        String nombre = "Alejandro"; // texto
+        boolean estudiante = true;   // verdadero o falso
+
+    
+    
+    
+    
     }
 }
